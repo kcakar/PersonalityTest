44d1bf2e0e51a9eb0f31ff18ddc0e0d13a1cac65
Keremcan Çakar <keremcan@hotmail.com.tr>
2019-07-13 16:07:02 +0300

    updullah

--- frontend/src/helpers/URLs.js
+++ frontend/src/helpers/URLs.js
@@ -1,6 +1,6 @@
 const server="https://personality-test-kcakar.herokuapp.com/api/v1";
 // const server="http://localhost:3001/api/v1";
-const webpage="https://triaakademi.com/";
+const webpage="http://www.bitatesti.com";
 
 const urls={
     webpage,
